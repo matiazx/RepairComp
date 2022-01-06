57ab6aee15c490170322b26400e0139dbcbde66d
matiaz <2201106@my.ipleiria.pt>
2022-01-06 22:56:16 +0000

    .
    tentativa de testes

--- backend/tests/functional/LoginCest.php
+++ backend/tests/functional/LoginCest.php
@@ -41,4 +41,15 @@ class LoginCest
         $I->dontSeeLink('Login');
         $I->dontSeeLink('Signup');
     }
+    public function tryLogin(FunctionalTester $I)
+    {
+        $I->amOnPage('/');
+        $I->click('Login');
+        $I->fillField('Username', 'Miles');
+        $I->fillField('Password', 'Davis');
+        $I->click('Enter');
+        $I->see('Hello, Miles', 'h1');
+        // $I->seeEmailIsSent(); // only for Symfony
+    }
+
 }
